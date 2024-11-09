e7bd52232187e1e5591c872e6db07b54c132010d
xdrila34 <148321805+xdrila34@users.noreply.github.com>
2024-11-09 12:48:08 +0100

    sets

---
+++ lesson3/conditionals.py
@@ -0,0 +1,13 @@
+age = 18
+if age >= 18:
+    print("you can vote")
+else:
+    print("you cannot vote")
+
+temperature = 28
+if temperature > 30:
+    print("its very hot outside")
+elif 20<= temperature <= 30:
+    print("the weather is pleasing")
+else:
+    print("its a cold day today")
