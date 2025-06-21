91b0ef5fef25c249b6e59c7892349e7ac64afd2b
Student <drilonlatifi9@gmail.com>
2025-06-21 10:45:55 +0000

    12

---
+++ lesson24/app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+st.title("Project Management App")
+
+st.header("Add a Developer")
+
+dev_name = st.text_input("Developer Name")
+dev_experience = st.number_input("Experience (Years)", min_value=0, max_value=50, value=0)
+
+if st.button("Create Developer"):
+    dev_data = {"name": dev_name, "experience": dev_experience}
+    response = requests.post(url="http://localhost:8000/developers/", json=dev_data)
+    st.json(response.json())
+
+st.header("Add a Project")
+proj_title = st.text_input("Project Title")
+proj_desc = st.text_area("Project Description")
+proj_lang = st.text_input("Languages Used (Comma-separated)")
+lead_dev_name = st.text_input("Lead Developer Name")
+lead_dev_exp = st.number_input("Lead Developer Experience (Years)", min_value=0, max_value=50, value=0)
+
+if st.button("Create Project"):
+    lead_dev_data = {"name": lead_dev_name, "experience": lead_dev_exp}
+    proj_data = {
+        "title": proj_title,
+        "description": proj_desc,
+        "language": proj_lang,
+        "lead_developer": lead_dev_data
+    }
+
+    response = requests.post(url="http://localhost:8000/projects/", json=proj_data)
+    st.json(response.json())
+st.header("Project Dashboard")
+if st.button("Get Project"):
+    response = requests.get("http://localhost:8000/projects/")
+    project_data = response.json()['projects']
+    if project_data:
+        projects_df = pd.DataFrame(project_data)
+
+        st.subheader("Project Overview")
+        st.dataframe(projects_df)
--- lesson24/main.py
+++ lesson24/main.py
@@ -10,3 +10,13 @@ def create_developer (developer: Developer):
 @app.post("/projects/")
 def create_project (project: Project):
     return{"message": "Project created successfully", "Project": project}
+
+@app.get("/projects/")
+def get_project():
+    sample_project = Project(
+        title = "sample project",
+        description = "this is a sample project",
+        language = ["python","javascript"],
+        lead_developer = Developer(name="Jon Doe", experience=5)
+    )
+    return {"Project":[sample_project]}
