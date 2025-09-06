f5aff832b65df116b661569a8eb0537d2a7d965c
Student <drilonlatifi9@gmail.com>
2025-09-06 12:27:34 +0200

    123

---
+++ perseritje/962025.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get ("/items/")
+def read_items():
+    return{"items":["item1","item2","item3"]}
+
+@app.post ("/items/")
+def create_items(name: str, price: float):
+    return{"item_name":name, "item_price":price}
