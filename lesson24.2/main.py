72cdada8199f0978d2e6cae298f70dc3c7cc9f3f
Student <drilonlatifi9@gmail.com>
2025-09-20 11:32:45 +0200

    123

---
+++ lesson24.2/main.py
@@ -0,0 +1,33 @@
+import sqlite3
+from tkinter.tix import INTEGER
+
+connection = sqlite3.connect('example.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    position TEXT NOT NULL,
+    department TEXT NOT NULL,
+    salary REAL
+)
+''')
+connection.commit()
+
+cursor.execute(
+    '''
+            INSERT INTO employees (name, position, department, salary)
+            VALUES (?, ?, ?, ?)
+    ''', ('John', 'Software Engineer', 'IT', 70000.00)
+
+
+)
+
+connection.commit()
+
+cursor.execute('SELECT * FROM employees')
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
